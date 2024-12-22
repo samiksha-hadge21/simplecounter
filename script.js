4796213ebb030dc24af24cd63e5db1b7538a154d
samiksha-hadge21 <samikshahadge21@gmail.com>
2024-12-22 18:03:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+ function Madd(){
+    let value= document.getElementById('count').innerHTML
+    value++;
+    document.getElementById('count').innerHTML=value
+     
+
+ }
+  
+
+ function Msub(){
+    let value = document.getElementById('count').innerHTML
+    value--;
+    document.getElementById('count').innerHTML=value
+    
+ }
+  
+
